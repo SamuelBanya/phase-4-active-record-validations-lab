8aeae99496f5fd755cd98f8f56a28288e245bf4f
Samuel Banya <sbanya@fastmail.com>
2022-10-23 13:41:41 -0500

    commit tree c1ae155d1ee690f328225d00fc04a6e6f7c014b3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait?
+
+    PATTERNS = [
+        /Won't believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+    def clickbait?
+        if PATTERNS.none? { |pattern| pattern.match title }
+            errors.add(:title, "must be clickbait, don't proceed")
+        end
+    end
 end
